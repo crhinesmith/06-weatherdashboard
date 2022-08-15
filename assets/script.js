16d93b165ad2bc9d05576f26c825c3791e61c7b0
ripbog <crhinesmith5@gmail.com>
2022-08-15 16:20:37 -0400

    Everything working except previous cities search

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ function renderSearchHistory (){
     //button EL.addevent listener ('click', new function that uses city as input)
     //button that calls the function with the city that is listed on the button
     //create an empty UL then append the search buttons to the UL
-    searchbtnEl.appendChild(buttonEl)
+    previousCities.append(buttonEl)
 
 
   })
@@ -40,16 +40,8 @@ function handleSearchFormSubmit(event) {
   // what if I do have a city then what?
   getCityCurrentWeather(city);
   saveCitySearch(city);
-
-
-
 }
 
-//one way to store the data found in the response from the URL is to have an undeclared variable up top then declare it in the function where the response is
-//one way to use the data found in the response that is done further down in the code is to have a function with parameters that youll pull from the response  
-// function xx(dat1, dat2) {
-// //     // do stuff
-// }
 
 
 
@@ -74,38 +66,27 @@ var getCityCurrentWeather = function (city) {
       console.log(cityLat);
       console.log(cityLong);
       getCityUVIWeather(cityLat, cityLong);
-      
-      var h2El = document.createElement('h3')
-      h2El.textContent = data.name
+
+      const currentDate = moment();
+      var h2El = document.createElement('h2');
+      h2El.textContent = data.name + " " + currentDate.format('MM-DD')
       today.appendChild(h2El);
 
       var tempEl = document.createElement('p')
-      tempEl.textContent = "temp: " + data.main.temp + " F"
+      tempEl.textContent = "Temp: " + data.main.temp + " F"
       today.appendChild(tempEl)
 
       var humEl = document.createElement('p')
-      humEl.textContent = "humidity: " + data.main.humidity + " %"
+      humEl.textContent = "Humidity: " + data.main.humidity + " %"
       today.appendChild(humEl)
 
       var windEl = document.createElement('p')
-      windEl.textContent = "wind: " + data.wind.speed + " MPH"
+      windEl.textContent = "Wind: " + data.wind.speed + " MPH"
       today.appendChild(windEl)
 
 
 
 
-      //need to grab the lat and long from the original queryURL then put in the lat and long on the URL firmin gave us in the slack becasue the UVI only available in that URL 
-
-      // once I have the data
-      // fetch('some uurl')
-      //         // .then(function(resp) {
-      //         //     return resp.json() // converting to json
-      //         // }).then(function (forecastData) {
-      //         //     console.log(forecastData) // json from line 43
-
-      //             // call a function and pass data to it
-      //             // xx(1, 2)
-      //         })
     })
 
 
@@ -123,6 +104,13 @@ var getCityUVIWeather = function (cityLat, cityLong) {
 
       var uviEl = document.createElement('p')
       uviEl.textContent = "UVI: " + data.current.uvi
+      if (data.current.uvi < 5){
+        uviEl.classList.add('bg-success')
+      } else if (data.current.uvi < 7){
+        uviEl.classList.add('bg-warning')
+      } else {
+        uviEl.classList.add('bg-danger')
+      }
       today.appendChild(uviEl)
       
       console.log(data.current.weather[0].icon)
@@ -130,6 +118,124 @@ var getCityUVIWeather = function (cityLat, cityLong) {
       iconEl.setAttribute('src',`https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
       )
       today.appendChild(iconEl)
+
+      var forecast1Card = document.createElement('div')
+      forecast1Card.classList.add('card', 'bg-dark', 'text-light','mb-3','p-3')
+
+      var forecast1Body = document.createElement('div');
+      forecast1Body.classList.add('card-body');
+      forecast1Card.append(forecast1Body);
+
+      const date1 = moment().add(1, 'days');
+      var date1El = document.createElement('h3');
+      date1El.textContent = date1.format('MM-DD')
+      
+      var temp1El = document.createElement('p')
+      temp1El.textContent = "Temp: " + data.daily[0].temp.day + " F"
+      var hum1El = document.createElement('p')
+      hum1El.textContent = "Humidity: " + data.daily[0].humidity + " %"
+      var wind1El = document.createElement('p')
+      wind1El.textContent = "Wind: " + data.daily[0].wind_speed + " MPH"
+      var icon1El = document.createElement('img')
+      icon1El.setAttribute('src',`https://openweathermap.org/img/w/${data.daily[0].weather[0].icon}.png`
+      )
+      
+      forecast1Card.append(date1El, temp1El, hum1El, wind1El, icon1El);
+
+      var forecast2Card = document.createElement('div')
+      forecast2Card.classList.add('card', 'bg-dark', 'text-light','mb-3','p-3')
+
+      var forecast2Body = document.createElement('div');
+      forecast2Body.classList.add('card-body');
+      forecast2Card.append(forecast2Body);
+
+      const date2 = moment().add(2, 'days');
+      var date2El = document.createElement('h3');
+      date2El.textContent = date2.format('MM-DD')
+      
+      var temp2El = document.createElement('p')
+      temp2El.textContent = "Temp: " + data.daily[1].temp.day + " F"
+      var hum2El = document.createElement('p')
+      hum2El.textContent = "Humidity: " + data.daily[1].humidity + " %"
+      var wind2El = document.createElement('p')
+      wind2El.textContent = "Wind: " + data.daily[1].wind_speed + " MPH"
+      var icon2El = document.createElement('img')
+      icon2El.setAttribute('src',`https://openweathermap.org/img/w/${data.daily[1].weather[0].icon}.png`
+      )
+      
+      forecast2Card.append(date2El, temp2El, hum2El, wind2El, icon2El);
+
+      var forecast3Card = document.createElement('div')
+      forecast3Card.classList.add('card', 'bg-dark', 'text-light','mb-3','p-3')
+
+      var forecast3Body = document.createElement('div');
+      forecast3Body.classList.add('card-body');
+      forecast3Card.append(forecast3Body);
+
+      const date3 = moment().add(3, 'days');
+      var date3El = document.createElement('h3');
+      date3El.textContent = date3.format('MM-DD')
+      
+      var temp3El = document.createElement('p')
+      temp3El.textContent = "Temp: " + data.daily[2].temp.day + " F"
+      var hum3El = document.createElement('p')
+      hum3El.textContent = "Humidity: " + data.daily[2].humidity + " %"
+      var wind3El = document.createElement('p')
+      wind3El.textContent = "Wind: " + data.daily[2].wind_speed + " MPH"
+      var icon3El = document.createElement('img')
+      icon3El.setAttribute('src',`https://openweathermap.org/img/w/${data.daily[2].weather[0].icon}.png`
+      )
+      
+      forecast3Card.append(date3El, temp3El, hum3El, wind3El, icon3El);
+
+      var forecast4Card = document.createElement('div')
+      forecast4Card.classList.add('card', 'bg-dark', 'text-light','mb-3','p-3')
+
+      var forecast4Body = document.createElement('div');
+      forecast4Body.classList.add('card-body');
+      forecast4Card.append(forecast4Body);
+
+      const date4 = moment().add(4, 'days');
+      var date4El = document.createElement('h3');
+      date4El.textContent = date4.format('MM-DD')
+      
+      var temp4El = document.createElement('p')
+      temp4El.textContent = "Temp: " + data.daily[3].temp.day + " F"
+      var hum4El = document.createElement('p')
+      hum4El.textContent = "Humidity: " + data.daily[3].humidity + " %"
+      var wind4El = document.createElement('p')
+      wind4El.textContent = "Wind: " + data.daily[3].wind_speed + " MPH"
+      var icon4El = document.createElement('img')
+      icon4El.setAttribute('src',`https://openweathermap.org/img/w/${data.daily[3].weather[0].icon}.png`
+      )
+      
+      forecast4Card.append(date4El, temp4El, hum4El, wind4El, icon4El);
+
+      var forecast5Card = document.createElement('div')
+      forecast5Card.classList.add('card', 'bg-dark', 'text-light','mb-3','p-3')
+
+      var forecast5Body = document.createElement('div');
+      forecast5Body.classList.add('card-body');
+      forecast5Card.append(forecast4Body);
+
+      const date5 = moment().add(5, 'days');
+      var date5El = document.createElement('h3');
+      date5El.textContent = date5.format('MM-DD')
+      
+      var temp5El = document.createElement('p')
+      temp5El.textContent = "Temp: " + data.daily[4].temp.day + " F"
+      var hum5El = document.createElement('p')
+      hum5El.textContent = "Humidity: " + data.daily[4].humidity + " %"
+      var wind5El = document.createElement('p')
+      wind5El.textContent = "Wind: " + data.daily[4].wind_speed + " MPH"
+      var icon5El = document.createElement('img')
+      icon5El.setAttribute('src',`https://openweathermap.org/img/w/${data.daily[4].weather[0].icon}.png`
+      )
+      
+      forecast5Card.append(date5El, temp5El, hum5El, wind5El, icon5El);
+
+
+      fiveDay.append(forecast1Card, forecast2Card, forecast3Card, forecast4Card, forecast5Card);
     })
 }
 // function handleAndget () {
